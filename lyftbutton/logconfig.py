bc900b1bc8ef3bd585dd1510b4d4fee38bdeedc4
Jair Trejo <jair@jairtrejo.mx>
2019-01-20 19:16:24 -0800

    Remove runtime dependency on colorama

--- lyftbutton/logconfig.py
+++ lyftbutton/logconfig.py
@@ -5,28 +5,6 @@ import structlog
 
 DEBUG = bool(os.getenv("AWS_SAM_LOCAL"))
 
-DEV_PROCESSORS = [
-    structlog.stdlib.add_log_level,
-    structlog.stdlib.add_logger_name,
-    structlog.stdlib.PositionalArgumentsFormatter(),
-    structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S.%f"),
-    structlog.processors.StackInfoRenderer(),
-    structlog.processors.format_exc_info,
-    structlog.dev.ConsoleRenderer(),
-]
-
-PROD_PROCESSORS = [
-    structlog.stdlib.filter_by_level,
-    structlog.stdlib.add_logger_name,
-    structlog.stdlib.add_log_level,
-    structlog.stdlib.PositionalArgumentsFormatter(),
-    structlog.processors.TimeStamper(fmt="iso"),
-    structlog.processors.StackInfoRenderer(),
-    structlog.processors.format_exc_info,
-    structlog.processors.UnicodeDecoder(),
-    structlog.processors.JSONRenderer(),
-]
-
 
 def logger_factory(*args):
     logger = structlog.stdlib.LoggerFactory()(*args)
@@ -35,8 +13,32 @@ def logger_factory(*args):
 
 
 def configure():
+    if DEBUG:
+        processors = [
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S.%f"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.dev.ConsoleRenderer(),
+        ]
+
+    else:
+        processors = [
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.processors.JSONRenderer(),
+        ]
+
     structlog.configure(
-        processors=DEV_PROCESSORS if DEBUG else PROD_PROCESSORS,
+        processors=processors,
         context_class=structlog.threadlocal.wrap_dict(dict),
         logger_factory=logger_factory,
     )
